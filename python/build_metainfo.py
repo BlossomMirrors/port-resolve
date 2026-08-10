2ef7426dd7d11d08a82d4493d63043a86ea7a655
koyu <me@koyu.space>
2026-08-11 01:12:41 +0200

    Fix metadata

--- python/build_metainfo.py
+++ python/build_metainfo.py
@@ -76,13 +76,31 @@ def build_metainfo(app_id: str, app_description: str, app_tag: str):
         + app_id
         + """</id>
   <metadata_license>FSFAP</metadata_license>
-  <project_license>LicenseRef-proprietary</project_license>
+  <project_license>LicenseRef-proprietary=https://d7umqicpi7263.cloudfront.net/eula/product/926e6a7e-46f0-465c-8182-ad56050c9094/d4d34663-7069-48ce-b0fa-57d3ddc00b6d.pdf</project_license>
   <name>"""
         + app_description
         + """</name>
-  <summary>Professional Editing, Color, Effects and Audio Post!</summary>
+  <developer id="com.blackmagic">
+    <name>Blackmagic Design Pty Ltd.</name>
+  </developer>
+  <summary>Professional editing, color grading, visual effects, and audio post production in one application</summary>
 
   <description>
+    <p>
+      DaVinci Resolve combines editing, color correction, visual effects, motion
+      graphics, and audio post production in a single application, so you can
+      go from the first cut to the final master without switching software.
+      It is used on Hollywood feature films, television shows, and commercials,
+      and now the same tools are available to editors on every kind of project.
+    </p>
+    <p>
+      The Cut and Edit pages provide fast, precise editing tools alongside
+      traditional trimming, while the Color page offers node based grading with
+      HDR support trusted by professional colorists. The Fusion page adds
+      Hollywood style visual effects and motion graphics, and the Fairlight page
+      brings a full digital audio workstation for mixing, editing, and mastering
+      sound, all within one project.
+    </p>
     <p>
       """
         + latest_description
@@ -96,24 +114,24 @@ def build_metainfo(app_id: str, app_description: str, app_tag: str):
 
   <screenshots>
     <screenshot type="default">
-      <caption>DaVinci Resolve 18 Cut Page</caption>
-      <image>https://images.blackmagicdesign.com/images/products/davinciresolve/overview/onesolution/carousel/cut.jpg</image>
+      <caption>DaVinci Resolve Cut Page</caption>
+      <image>https://cdn.blossomos.org/forgeassets/6544d7e8-7106-48a6-b28c-3e9f0c12f6c6.png</image>
     </screenshot>
     <screenshot>
-      <caption>DaVinci Resolve 18 Edit Page</caption>
-      <image>https://images.blackmagicdesign.com/images/products/davinciresolve/overview/onesolution/carousel/edit.jpg</image>
+      <caption>DaVinci Resolve Edit Page</caption>
+      <image>https://cdn.blossomos.org/forgeassets/a32844aa-f01f-47f7-98dc-fd7a1c81b523.jpg</image>
     </screenshot>
     <screenshot>
-      <caption>DaVinci Resolve 18 Color Page</caption>
-      <image>https://images.blackmagicdesign.com/images/products/davinciresolve/overview/onesolution/carousel/color.jpg</image>
+      <caption>DaVinci Resolve Color Page</caption>
+      <image>https://cdn.blossomos.org/forgeassets/999bbd61-f440-4e35-981e-a4bd6c0a9872.jpg</image>
     </screenshot>
     <screenshot>
-      <caption>DaVinci Resolve 18 Fusion Page</caption>
-      <image>https://images.blackmagicdesign.com/images/products/davinciresolve/overview/onesolution/carousel/fusion.jpg</image>
+      <caption>DaVinci Resolve Fusion Page</caption>
+      <image>https://cdn.blossomos.org/forgeassets/62d204e2-6dea-4e83-ab83-cff2864adf69.jpg</image>
     </screenshot>
     <screenshot>
-      <caption>DaVinci Resolve 18 Fairlight Page</caption>
-      <image>https://images.blackmagicdesign.com/images/products/davinciresolve/overview/onesolution/carousel/fairlight.jpg</image>
+      <caption>DaVinci Resolve Fairlight Page</caption>
+      <image>https://cdn.blossomos.org/forgeassets/4fef76c5-a614-4090-bd6d-398e9b3c7e13.jpg</image>
     </screenshot>
   </screenshots>
 
